0357b944b5032ca60106d2ff8fdb580073d7cc89
Daniel Lucazeau <contact@dlucazeau.fr>
2015-01-15 14:53:13 +0100

    BDD sur localhost

--- src/fr/cursusSopra/tech/PostgresConnection.java
+++ src/fr/cursusSopra/tech/PostgresConnection.java
@@ -7,9 +7,9 @@ public class PostgresConnection {
 	public static Connection GetConnexion() {
         try {
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://s2.neggruda.net/glougloubar";
-            String user = "cursussopra";
-            String passwd = "cursussopra";
+            String url = "jdbc:postgresql://localhost/glougloubar";
+            String user = "postgres";
+            String passwd = "postgres";
             return DriverManager.getConnection(url, user, passwd);
         } catch (Exception e) {
             e.printStackTrace();
